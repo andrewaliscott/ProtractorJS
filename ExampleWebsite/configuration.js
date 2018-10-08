f85b386fd97b9b7a9c84d79c74e26e9942136958
andrewaliscott <42274741+andrewaliscott@users.noreply.github.com>
2018-10-08 17:58:53 -0400

    Update configuration.js

--- ExampleWebsite/configuration.js
+++ ExampleWebsite/configuration.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['submissionofForm.js'],
+    specs: ['spec.js'],
     capabilities: {
         'browserName': 'chrome'
     }
